f0740df0b03dc1ade3cd2097980588e4663f6394
Jabes <jabeslima15@gmail.com>
2024-11-18 14:01:58 -0300

    Mensagem descrevendo as alterações feitas

--- src/pages/Pages_normal/Cards.jsx
+++ src/pages/Pages_normal/Cards.jsx
@@ -44,11 +44,16 @@ export default function Cards() {
     const [date, setDate] = useState("Carregrando...");
     const [procurar, setProcurar] = useState("")
     const [loading, setLoading] = useState(true);
-    const clanTag = '#LOV8UVO2';
-    const encodedTag = encodeURIComponent(clanTag);
+    // const clanTag = '#LOV8UVO2';
+    // const encodedTag = encodeURIComponent(clanTag);
+    const VITE_API_KEY ="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjU5ZDRlNTJmLTc5YWUtNGI3Yy05Y2RmLTFlOTFiNmVlMGExZSIsImlhdCI6MTczMTc3OTk0NCwic3ViIjoiZGV2ZWxvcGVyLzRhOTk3YWE3LTEzMjgtMzY0Zi0zNWJhLTYxNzczYzg3ODdmNiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjIwMC4yMTUuMTY0LjgxIl0sInR5cGUiOiJjbGllbnQifV19.X01Epz8mbACCGaYb1xVDNEQQMYj6OsQBNdU_FJumpHYfyLzYjcPfQueApglNfMm-_4OJNCbTxZWkMqsQrJ5UCA"
 
     useEffect(() => {
-      axios.get(`/api/clans/${encodedTag}/members`)
+      axios.get(`https://api.clashofclans.com/v1/clans/%232LOV8UVO2/members`, {
+        headers: {
+            Authorization: `Bearer ${VITE_API_KEY}`
+        }
+      })
         .then(response => {
           setDate(response.data);
           setLoading(false);
